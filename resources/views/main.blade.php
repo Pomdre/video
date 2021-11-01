bbf6b1df390fffc3c02d6391d70bd6e6ba0e793e
Pomdre <bataldensondre@gmail.com>
2021-11-01 18:41:51 +0100

    up

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,18 +8,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="storage/css/main.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/freezeframe@3.0.10/build/css/freezeframe_styles.min.css">
+    <script type="text/javascript" src="https://unpkg.com/freezeframe@3.0.10/build/js/freezeframe.pkgd.min.js"></script>
+  
 </head>
 <body>
 <div class="container">
   <div class="row">
 @foreach($static as $data)
    <div class="col-md-10">
-    <a href="view?file={{$data->basename}}"><img class="img-fluid static" src="storage/static/{{$data->static}}"></a>
-    <img class="img-fluid active" src="storage/gif/{{$data->gif}}">
+    {{-- <a href="view?file={{$data->basename}}"><img class="img-fluid static" src="storage/static/{{$data->static}}"></a>
+    <img class="img-fluid active" src="storage/gif/{{$data->gif}}"> --}}
+    <img class="freezeframe" src="storage/gif/{{$data->gif}}" /> 
 </div>
 @endforeach
 <div style="margin-top: 10px;"></div>
   </div>
 </div>
+<script>$(function() {
+  ff = new freezeframe().freeze();
+})
+</script>
 </body>
 </html>
