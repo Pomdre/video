2c46e85630eede468692abeb168d08d0d89922d3
Pomdre <bataldensondre@gmail.com>
2021-11-06 12:25:46 +0100

    faster

--- public/storage/gif.sh
+++ public/storage/gif.sh
@@ -1,5 +1,5 @@
 for file in video/*; do
     echo "$(basename "${file%%.*}").gif"
     echo "$(basename "${file}")"
-    ffmpeg -n -ss 30 -t 12 -i "video/$(basename "${file}")" -vf "fps=10,scale=1080:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 "gif/$(basename "${file%%.*}").gif"
+    ffmpeg -n -ss 30 -t 20 -i "video/$(basename "${file}")" -vf "setpts=0.2*PTS,fps=30,scale=1080:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 "gif/$(basename "${file%%.*}").gif"
 done
