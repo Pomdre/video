e3702d6844d30c5e38cb2f0574ae4f55909f713d
Pomdre <bataldensondre@gmail.com>
2024-05-22 22:43:42 +0200

    test style

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -10,25 +10,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unpkg.com/freezeframe@3.0.10/build/css/freezeframe_styles.min.css">
     <script type="text/javascript" src="https://unpkg.com/freezeframe@3.0.10/build/js/freezeframe.pkgd.min.js"></script>
-  
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body style="background-color: black">
-<div class="container">
-  <a href="./sort"><button>Sort by votes</button></a>
-  <a href="./all"><button>All</button></a>
-  <div class="row">
-@foreach($static as $data)
-   <div class="col-md-10">
-    {{-- <a href="view?file={{$data->basename}}"><img class="img-fluid static" src="storage/static/{{$data->static}}"></a>
-    <img class="img-fluid active" src="storage/gif/{{$data->gif}}"> --}}
-    <img class="freezeframe freezeframe-responsive" src="storage/gif/{{"$data->gif"}}" />
-    <a style="margin-left: 90%" href="view?file={{$data->basename}}">Vis</a>
-</div>
-@endforeach
-<div style="margin-top: 10px;"></div>
+<body style="background-color: black;">
+  <div class="container text-white">
+    <div class="my-3">
+      <a href="./sort" class="btn btn-primary">Sort by votes</a>
+      <a href="./all" class="btn btn-secondary">All</a>
+    </div>
+    <div class="row">
+      @foreach($static as $data)
+        <div class="col-12 col-md-6 mb-4">
+          {{-- <a href="view?file={{$data->basename}}"><img class="img-fluid static" src="storage/static/{{$data->static}}"></a>
+          <img class="img-fluid active" src="storage/gif/{{$data->gif}}"> --}}
+          <img class="freezeframe freezeframe-responsive img-fluid" src="storage/gif/{{"$data->gif"}}" />
+          <a class="d-block text-right mt-2 text-white" href="view?file={{$data->basename}}">Vis</a>
+        </div>
+      @endforeach
+    </div>
+    <div class="mt-3">
+      <button class="btn btn-warning float-right""><a href="./">Reolad site</a></button>
+    </div>
   </div>
-</div>
-<button style="float: right;"><a href="./">Reolad site</a></button>
+</body>
 <script>$(function() {
   ff = new freezeframe().freeze();
 })
